2f6a9172ec0f57e51b7427f27f686c8f02e1d054
Michael <migueljmiller@gmail.com>
2016-12-01 16:59:05 -0800

    add person objects

---
+++ js/myJS.js
@@ -0,0 +1,25 @@
+var bob = {wish_list = "wish list"};
+var carol = {wish_list = "wish list"};
+var dan = {wish_list = "wish list"};
+var holly = {wish_list = "wish list"};
+var jaimie = {wish_list = "wish list"};
+var jamaal = {wish_list = "wish list"};
+var josh = {wish_list = "wish list"};
+var karli = {wish_list = "wish list"};
+var lauren = {wish_list = "wish list"};
+var mark = {wish_list = "wish list"};
+var michael = {wish_list = "wish list"};
+var mikeW = {wish_list = "wish list"};
+var mikie = {wish_list = "wish list"};
+var pat = {wish_list = "wish list"};
+
+
+var santa_list = {bob, carol, dan, holly, jaimie, jamaal, josh, karli, lauren, mark, michael, mikeW, mikie, pat};
+
+function person {first, wish) {
+	this.firstName = first;
+	this,wishList = wish;
+}
+
+// var bob = new person ("bob","wishlist");
+	
