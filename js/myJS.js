1764aa80d28e78895d64a59118d40102c9b4c73c
Michael <migueljmiller@gmail.com>
2016-12-02 21:51:55 -0800

    add js person object constructor; change tags

--- js/myJS.js
+++ js/myJS.js
@@ -1,25 +1,37 @@
-var bob = {wish_list = "wish list"};
-var carol = {wish_list = "wish list"};
-var dan = {wish_list = "wish list"};
-var holly = {wish_list = "wish list"};
-var jaimie = {wish_list = "wish list"};
-var jamaal = {wish_list = "wish list"};
-var josh = {wish_list = "wish list"};
-var karli = {wish_list = "wish list"};
-var lauren = {wish_list = "wish list"};
-var mark = {wish_list = "wish list"};
-var michael = {wish_list = "wish list"};
-var mikeW = {wish_list = "wish list"};
-var mikie = {wish_list = "wish list"};
-var pat = {wish_list = "wish list"};
+// var bob = {wish_list = "wish list"};
+// var carol = {wish_list = "wish list"};
+// var dan = {wish_list = "wish list"};
+// var holly = {wish_list = "wish list"};
+// var jaimie = {wish_list = "wish list"};
+// var jamaal = {wish_list = "wish list"};
+// var josh = {wish_list = "wish list"};
+// var karli = {wish_list = "wish list"};
+// var lauren = {wish_list = "wish list"};
+// var mark = {wish_list = "wish list"};
+// var michael = {wish_list = "wish list"};
+// var mikeW = {wish_list = "wish list"};
+// var mikie = {wish_list = "wish list"};
+// var pat = {wish_list = "wish list"};
 
+// {bob, carol, dan, holly, jaimie, jamaal, josh, karli, lauren, mark, michael, mikeW, mikie, pat};
 
-var santa_list = {bob, carol, dan, holly, jaimie, jamaal, josh, karli, lauren, mark, michael, mikeW, mikie, pat};
+var santaList = [];
 
 function person {first, wish) {
 	this.firstName = first;
 	this.wishList = wish;
+};
+
+function makeAdd (first, wish, santaList) {
+
 }
 
-// var bob = new person ("bob","wishlist");
-	
+function viewList (santaList, name) {
+	for (i=0; i < santaList.length; i++) {
+		if name == i.firstName {
+			return i.wishList;	
+		};
+
+	};
+
+};
